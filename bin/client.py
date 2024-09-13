1bc10f43b7c4b453d26cbe015417e45e73812149
Anudeep Kalitkar <50821904+anudeepkalitkar@users.noreply.github.com>
2024-09-13 12:01:20 -0400

    Added modules to init.py

--- bin/client.py
+++ bin/client.py
@@ -1,6 +1,7 @@
 import zenduty
 from zenduty.exceptions import ApiException
-
+from zenduty import apiV2
+from zenduty.apiV2 import APIException
 api_instance = zenduty.TeamsApi(zenduty.ApiClient("ENTER-YOUR-ACCESS-TOKEN-HERE"))
 
 try:
@@ -8,4 +9,13 @@ try:
     api_response = api_instance.get_teams()
     print(api_response.data)
 except ApiException as e:
+    print("Exception when calling TeamsApi->api_account_teams_get: %s\n" % e)
+
+apiv2_instance = apiV2.TeamsClient(apiV2.ZendutyClient(apiV2.ZendutyCredential("ENTER-YOUR-ACCESS-TOKEN-HERE")))
+
+try:
+    # Get Teams
+    api_response = apiv2_instance.list_teams()
+    print(api_response.data)
+except APIException as e:
     print("Exception when calling TeamsApi->api_account_teams_get: %s\n" % e)
---
+++ zenduty/apiV2/__init__.py
@@ -0,0 +1,10 @@
+from .client import ZendutyClient
+from .client import ZendutyCredential
+from .client import APIException
+from .accounts.members import AccountMemberClient
+from .accounts.roles import AccountRoleClient
+from .events import EventClient
+from .incidents import IncidentClient
+from .teams import TeamsClient
+
+
